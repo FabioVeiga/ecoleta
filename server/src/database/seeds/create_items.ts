b69851492848661336d560891b6a72ee1943a4e5
Fabio Veiga <droidbinho@gmail.com>
2020-06-03 19:38:04 -0300

    FIXBUG - Ajuste na entidade ITEMS alterado attr name para title

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -4,7 +4,7 @@ export async function seed(knex: Knex) {
     await knex('items').insert([
         {title: 'Lâmpadas', image: 'lampadas.svg'},
         {title: 'Pilhas e baterias', image: 'baterias.svg'},
-        {title: 'Papéis e Papalão', image: 'papeis-papelao.svg'},
+        {title: 'Papéis e Papelão', image: 'papeis-papelao.svg'},
         {title: 'Resíduos Eletrônicos', image: 'eletronico.svg'},
         {title: 'Resíduos Orgânicos', image: 'organico.svg'},
         {title: 'Óleo de Cozinha', image: 'oleo.svg'}
