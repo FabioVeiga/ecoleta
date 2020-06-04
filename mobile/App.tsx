81c31665c179bccb2b1eac31d892f7f7005b60e8
Fabio Veiga <droidbinho@gmail.com>
2020-06-04 16:36:14 -0300

    add font-google

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,31 @@
+//Packages
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo'
+import { StatusBar } from 'react-native'
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
+
+//Paginas
+import Home from './src/pages/home'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+  //useFont sera carregada antes ded tudo
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  //se nao carregou, vai monstrar um spinning
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+  //nao consegue retirnar dois ou mais componentes no return
+  //deve retonar uma VIEW ou utilizar o Fragment <>
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Home />
+    </>
+  )
+}
